33a400f1268c63c6889aa42b4349719e5184243a
Alex V <posdata.5@hotmail.com>
2017-12-12 13:27:29 +0000

    Added error messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,20 @@ class ContactsController < ApplicationController
     def new
       @contact = Contact.new
     end
+    
+    
     def create
       @contact = Contact.new(contact_params)
       if @contact.save
-         redirect_to new_contact_path, notice: "Mensaje enviado."
+         flash[:success] = "Message sent."
+         redirect_to new_contact_path
       else
-         redirect_to new_contact_path, notice: "Hubo un error"
+         flash[:error] = @contact.errors.full_messages.join(", ")
+         redirect_to new_contact_path
       end
     end
+    
+    
     private
       def contact_params
          params.require(:contact).permit(:name, :email, :comments)
