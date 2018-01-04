99bfbe62b48d52438b086a796f53b892187f5f2c
Alex V <posdata.5@hotmail.com>
2018-01-04 06:54:53 +0000

    Added plans table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Plan.create(name: 'basic', price: 0)
+Plan.create(name: 'pro', price: 10)
