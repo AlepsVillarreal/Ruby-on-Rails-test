c00fa51ea7b74b7696ab5aa46c3d1a5005104aea
Alex V <posdata.5@hotmail.com>
2018-01-14 00:54:01 +0000

    Added profiles controller, new view and link on homepage

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+    
+    #Cuando un usuario haga un GET request a /users/users_id/profile/new
+    
+    def new
+        #Render a blank profile detail form
+        
+    end
+    
+end
