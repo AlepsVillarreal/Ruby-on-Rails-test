c4240b76e2f701a6decdbb2353ba205c26c4248e
Alex V <posdata.5@hotmail.com>
2018-01-18 17:56:08 +0000

    Added users index action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user! #Para que no cualquier usuario pueda ver los usuarios. Ahora tienes que estar loggeado
+  
+  def index
+  
+  end
+  
   # GET to /users/:id
   def show
     @user = User.find( params[:id] )
