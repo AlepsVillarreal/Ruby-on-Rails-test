8c4ab49efa559a6cafb7db6b6be1ef350ce9a6b6
Alex <androreal.av@gmail.com>
2018-06-15 22:53:11 +0000

    Las primeras modificciones en clase

--- db/schema.rb
+++ db/schema.rb
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 20180115180405) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "plan_id"
-    t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
